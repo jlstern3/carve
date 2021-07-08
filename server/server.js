20552e1d77c18da9adc0a54cec8f68b727aafede
Jessica Stern <jlstern3@gmail.com>
2021-07-08 13:19:18 -0400

    Server actively listening on Port 8000

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require ('express');
+const app = express();
+const cors = require ('cors');
+const port = 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+require('./config/mongoose.config');
+// require('./routes/riders.route');
+
+app.listen(port, () => {
+    console.log(`Server is actively listening on Port ${port}`)
+});

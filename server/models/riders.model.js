dadbc8d1cee2219beadc1e6e3f31c8e7d830851e
Jessica Stern <jlstern3@gmail.com>
2021-07-09 22:40:37 -0400

    RidersSchema created with validations

---
+++ server/models/riders.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const RiderSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        minlength: [3, "Name must be at least three characters long."]
+    },
+    age: {
+        type: Number,
+        required: [true, "Name is required"],
+    },
+    hometown: {
+        type: String,
+        required: [true, "Name is required"],
+        minlength: [3, "Hometown be at least three characters long."]
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model("Rider", RiderSchema);

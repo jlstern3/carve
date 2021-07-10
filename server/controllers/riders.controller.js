bbf20adb083cf605df760f6881b81c64eec7b36e
Jessica Stern <jlstern3@gmail.com>
2021-07-10 15:52:49 -0400

    Controllers staged, not tested

--- server/controllers/riders.controller.js
+++ server/controllers/riders.controller.js
@@ -26,3 +26,50 @@ module.exports.getAllRiders = (req, res) => {
             res.json(err);
         });
 }
+
+
+
+
+// module.exports.getSingleRider = (req, res) => {
+//     console.log("Inside getSingleRider");
+//     Rider.findById(req.params.id)
+//         .then((singleRider) => {
+//             console.log(singleRider);
+//             res.json(singleRider);
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//             res.json(err);
+//         });
+// }
+
+
+// module.exports.deleteRider = (req,res) =>{
+//     console.log("Inside deleteRider");
+//     Rider.findByIdAndDelete(req.params.id)
+//         .then((deletedRider) =>{
+//             console.log(deletedRider);
+//             res.json(deletedRider);
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//             res.json(err);
+//         });
+// }
+
+
+// module.exports.updateRider = (req, res) =>{
+//     console.log("Inside updateRider");
+//     Rider.findByIdAndUpdate(req.params.id, req.body, {
+//         new: true,
+//         runValidators: true
+//     })
+//         .then((updatedRider) => {
+//             console.log(updatedRider);
+//             res.json(updatedRider);
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//             res.json(err);
+//         })
+// }

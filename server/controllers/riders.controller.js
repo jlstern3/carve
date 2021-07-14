4a6a683b4ef5c82efbb4b4610b604a877d2e7f65
Jessica Stern <jlstern3@gmail.com>
2021-07-13 22:42:15 -0400

    Create Rider route works

--- server/controllers/riders.controller.js
+++ server/controllers/riders.controller.js
@@ -1,18 +1,18 @@
 const Rider = require('../models/riders.model');
 
-// module.exports.createRider = (req,res) => {
-//     console.log("Inside createRider");
-//     console.log(req.body);
-//     Rider.create(req.body)
-//         .then((newRider) =>{
-//             console.log(newRider);
-//             res.json(newRider);
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//             res.json(err);
-//         });
-// }
+module.exports.createRider = (req,res) => {
+    console.log("Inside createRider");
+    console.log(req.body);
+    Rider.create(req.body)
+        .then((newRider) =>{
+            console.log(newRider);
+            res.json(newRider);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.json(err);
+        });
+}
 
 module.exports.getAllRiders = (req, res) => {
     console.log("inside getAllRiders");
--- server/routes/riders.route.js
+++ server/routes/riders.route.js
@@ -2,7 +2,7 @@ const RiderController = require("../controllers/riders.controller");
 
 module.exports = function(app) {
 
-        // app.post('/api/riders', RiderController.createRider);
+        app.post('/api/riders', RiderController.createRider);
 
         app.get('/api/riders', RiderController.getAllRiders);
 

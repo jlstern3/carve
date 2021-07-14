ed9d706c62bb5e73efc1af548a2b29d302f52684
Jessica Stern <jlstern3@gmail.com>
2021-07-14 18:27:00 -0400

    UserSchema Password val. and encrypt created

--- server/models/users.models.js
+++ server/models/users.models.js
@@ -20,5 +20,37 @@ const UserSchema = new mongoose.Schema({
     },
 }, {timestamps: true});
 
+
+
+UserSchema.virtual("confirmPassword")
+    .get(() => this._confirmPassword)
+    .set((value) => this._confirmPassword = value);
+
+
+
+UserSchema.pre("validate", function(next){
+    if (this.password !== this.confirmPassword) {
+        console.log(this.password);
+        console.log(this.confirmPassword);
+        this.invalidate("confirmPassword", "Passwords didn't match.  Please try again.");
+    }
+
+    next();
+})
+
+
+UserSchema.pre("save", function(next) {
+    bcrypt.hash(this.password, 10)
+        .then((hashedPassword) => {
+            console.log("Password: " + this.passowrd);
+            console.log("Hashed: " + hashedPassword);
+            this.password = hashedPassword;
+            next();
+        })
+        .catch
+})
+
+
 const User = mongoose.model("User", UserSchema);
+
 module.exports = User;

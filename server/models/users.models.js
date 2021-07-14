982fca4b90983a25b9f0fd0e48697a8bde22f566
Jessica Stern <jlstern3@gmail.com>
2021-07-14 17:52:17 -0400

    User modularization and UserSchema created

---
+++ server/models/users.models.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required."],
+    },
+    name: {
+        type: String,
+        required: [true, "Name is required."],
+        minlength: 2,
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required."],
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required."],
+    },
+}, {timestamps: true});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;

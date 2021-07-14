f292032ea87674728075c211b1160fcbcd8cb809
Jessica Stern <jlstern3@gmail.com>
2021-07-14 18:28:19 -0400

    User login route created

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,6 +1,6 @@
 const UserController = require('../controllers/users.controllers');
 
 module.exports = function (app) {
-    //register user
     app.post("/api/users/register", UserController.register);
+    app.post("/api/users/login", UserController.login);
 }

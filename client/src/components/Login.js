7aa13ddcb8a8b9ba9834c2e47e0709c0eefc315c
Jessica Stern <jlstern3@gmail.com>
2021-11-11 21:47:24 -0500

    Login component built out, NOT tested

---
+++ client/src/components/Login.js
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import {navigate} from '@reach/router';
+
+const Login = () => {
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [errorMessage, setErrorMessage] = useState("");
+
+const login = event => {
+    e.preventDefault();
+    axios.post('http://localhost:8000/api/users/login', {
+        email: email,
+        password: password,
+    },
+    {
+        withCredentials: true,
+    })
+    .then((res)=>{
+        console.log(res.cookie);
+        console.log(res);
+        console.log(res.data, "this is res data");
+        navigate('/api/riders');
+    })
+    .catch((err)=>{
+        console.log(err.response);
+        setErrorMessage(err.response.data.message);
+    });
+};
+
+return(
+    <div>
+        <h3>Login</h3>
+        <p className="error-text">{errorMessage ? errorMessage : ""}</p>
+        <form onSubmit = {login}>
+            <div>
+                <label>Email</label>
+                <input
+                type="text"
+                name="email"
+                value={email}
+                onChange = {(e)=> setEmail(e.target.value)} />
+            </div>
+            <div>
+                <label>Password</label>
+                <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={(e)=> setPassword(e.target.value)}/>
+            </div>
+            <button type="submit">Sign In</button>
+        </form>
+    </div>
+);
+};
+
+export default Login;

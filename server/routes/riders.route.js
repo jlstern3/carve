42ae722357a76a86f2ee0c5a54746a86a24ab7a0
Jessica Stern <jlstern3@gmail.com>
2021-11-11 21:48:17 -0500

    Cleaned up blank lines

--- server/routes/riders.route.js
+++ server/routes/riders.route.js
@@ -1,15 +1,10 @@
 const RiderController = require("../controllers/riders.controller");
 
 module.exports = function(app) {
-
         app.post('/api/riders', RiderController.createRider);
-
         app.get('/api/riders', RiderController.getAllRiders);
-
         app.get('/api/riders/:id', RiderController.getSingleRider);
-
         app.delete('/api/riders/:id', RiderController.deleteRider);
-
         app.put('/api/riders/:id', RiderController.updateRider);
 
 

a7fdad008481eb419f0a318eafe451a8196960a6
Jessica Stern <jlstern3@gmail.com>
2021-12-19 13:07:05 -0500

    Update Profile pg linked to from dashboard

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react"
 import { Form, Button, Card, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 export default function Login() {
     const emailRef = useRef()
@@ -9,6 +9,7 @@ export default function Login() {
     const { login } = useAuth()
     const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
 
     async function handleSubmit(e) {
         e.preventDefault();
@@ -16,7 +17,8 @@ export default function Login() {
             setError("")
             setLoading(true)
             await login(emailRef.current.value, passwordRef.current.value)
-            // history.push("/")
+            // brings us to dashboard upon successful login
+            navigate.push("/")
         } catch {
             setError("Failed to log in.")
         }

73ddfda6adec66688cec95119fc48a52b9bdfa4b
Jessica Stern <jlstern3@gmail.com>
2021-12-19 11:56:16 -0500

    Signup and login functions work

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -23,7 +23,7 @@ export default function Signup() {
             await signup(emailRef.current.value, passwordRef.current.value)
             // history.push("/")
         } catch {
-            setError("Failed to create an account")
+            setError("Failed to create an account.")
         }
 
         setLoading(false)
--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -17,7 +17,9 @@ export default function AuthProvider({ children }) {
         return auth.createUserWithEmailAndPassword(email, password)
     }
 
-
+    function login (email, password){
+        return auth.signInWithEmailAndPassword(email, password)
+    }
     // sets current user to whoever logged in -- Firebase notifies us when 
     // something has changed so we're saying if something has changed, set that user to the current user
     // only want this to render once, hence the useEffect
@@ -32,7 +34,7 @@ export default function AuthProvider({ children }) {
 
     const value = {
         currentUser,
-        // login,
+        login,
         signup,
         // logout,
         // resetPassword,

a8acacfc21f97accb7d974dce1c67740f110e656
Jessica Stern <jlstern3@gmail.com>
2021-12-21 21:01:24 -0500

    PrivateRoute created but not functioning

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom'
+import { useAuth } from '../contexts/AuthContext';
+
+export default function PrivateRoute({ component: Component, ...rest }) {
+    const { currentUser } = useAuth()
+    return (
+        <Routes>
+            <Route
+                {...rest}
+                render={props => {
+                    return currentUser ? <Component {...props} /> :
+                        <Navigate to="/login" />
+                }}>
+            </Route>
+        </Routes>
+    )
+}
